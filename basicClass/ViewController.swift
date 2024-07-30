f26c5e8ff4be160162bf41eb7cc19dc9c2e9d89d
LeeJinKyu <jade981220@gmail.com>
2024-07-31 07:52:03 +0900

    라벨 클로저 스타일로 코드 변경

--- basicClass/ViewController.swift
+++ basicClass/ViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 class ViewController: UIViewController {
 
-    let helloLabel = UILabel()
-    let blueView = UIView()
+    let helloLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, World"
+        return label
+    }()
+    //let blueView = UIView()
     let myNameButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -28,28 +33,19 @@ class ViewController: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         view.backgroundColor = .red
+        //주어진 코드에서 override func viewWillAppear(_ animated: Bool) 메서드는 뷰 컨트롤러가 화면에 나타나기 직전에 호출되는 메서드입니다. 이 메서드는 보통 화면이 보이기 직전에 수행해야 할 작업들을 정의할 때 사용됩니다
     }
     func configureView() {
         helloLabel.text = "Hello, World"
         //
         view.addSubview(helloLabel)
-        // 2
-        helloLabel.translatesAutoresizingMaskIntoConstraints = false
-        // 3
         NSLayoutConstraint.activate([
             helloLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             helloLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
         ])
 
-        blueView.backgroundColor = .blue
-        view.addSubview(blueView)
-        blueView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            blueView.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -150),
-            blueView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            blueView.widthAnchor.constraint(equalToConstant: 100),
-            blueView.heightAnchor.constraint(equalToConstant: 40)
-        ])
+        myNameButton.backgroundColor = .white
+
         view.addSubview(myNameButton)
         NSLayoutConstraint.activate([
             myNameButton.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -150),
