558a98ab269c1399d611bdf470e502b76f8611a1
LeeJinKyu <jade981220@gmail.com>
2024-07-31 02:39:46 +0900

    1

--- basicClass/ViewController.swift
+++ basicClass/ViewController.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-
+  let helloLabel = UILabel()
+  let blueView = UIView()
+  let myNameButton = UIButton()
+  let temp = UITableView()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemPink
+    configureView()
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    view.backgroundColor = .red
+  }
+  func configureView() {
+    helloLabel.text = "Hello, World"
+    //
+    view.addSubview(helloLabel)
+    // 2
+    helloLabel.translatesAutoresizingMaskIntoConstraints = false
+    // 3
+    NSLayoutConstraint.activate([
+      helloLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+      helloLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+    ])
+    blueView.backgroundColor = .blue
+    view.addSubview(blueView)
+    blueView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      blueView.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -150),
+      blueView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      blueView.widthAnchor.constraint(equalToConstant: 100),
+      blueView.heightAnchor.constraint(equalToConstant: 40)
+    ])
+  }
 }
-
