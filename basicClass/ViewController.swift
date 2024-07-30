7e308982d667cefb0abd3cc6591ed0be6bc4a54f
LeeJinKyu <jade981220@gmail.com>
2024-07-31 07:32:20 +0900

    버튼클릭시 라벨 글씨 변경

--- basicClass/ViewController.swift
+++ basicClass/ViewController.swift
@@ -7,38 +7,60 @@
 
 import UIKit
 class ViewController: UIViewController {
-  let helloLabel = UILabel()
-  let blueView = UIView()
-  let myNameButton = UIButton()
-  let temp = UITableView()
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    view.backgroundColor = .systemPink
-    configureView()
-  }
-  override func viewWillAppear(_ animated: Bool) {
-    view.backgroundColor = .red
-  }
-  func configureView() {
-    helloLabel.text = "Hello, World"
-    //
-    view.addSubview(helloLabel)
-    // 2
-    helloLabel.translatesAutoresizingMaskIntoConstraints = false
-    // 3
-    NSLayoutConstraint.activate([
-      helloLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-      helloLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-    ])
-    blueView.backgroundColor = .blue
-    view.addSubview(blueView)
-    blueView.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-      blueView.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -150),
-      blueView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      blueView.widthAnchor.constraint(equalToConstant: 100),
-      blueView.heightAnchor.constraint(equalToConstant: 40)
-    ])
-  }
-}
+
+    let helloLabel = UILabel()
+    let blueView = UIView()
+    let myNameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Press me", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    let temp = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+        configureView()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .red
+    }
+    func configureView() {
+        helloLabel.text = "Hello, World"
+        //
+        view.addSubview(helloLabel)
+        // 2
+        helloLabel.translatesAutoresizingMaskIntoConstraints = false
+        // 3
+        NSLayoutConstraint.activate([
+            helloLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            helloLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+
+        blueView.backgroundColor = .blue
+        view.addSubview(blueView)
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            blueView.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -150),
+            blueView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            blueView.widthAnchor.constraint(equalToConstant: 100),
+            blueView.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        view.addSubview(myNameButton)
+        NSLayoutConstraint.activate([
+            myNameButton.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -150),
+            myNameButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myNameButton.widthAnchor.constraint(equalToConstant: 100),
+            myNameButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+      @objc func buttonTapped() {
+            helloLabel.text = "Leeo"
+
+        }
+    }
+
